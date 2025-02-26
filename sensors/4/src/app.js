b5b5d037f5e145473a4c957de194ae5bbc570d59
Leonardo Angelucci <mail@leonardo-angelucci.ch>
2025-02-26 09:23:23 +0100

    update

--- sensors/4/src/app.js
+++ sensors/4/src/app.js
@@ -7,6 +7,10 @@ document.body.addEventListener("touchstart", function (e) {
 
 document.body.addEventListener("touchend", function (e) {
   let count = e.touches.length;
-  document.body.textContent = "0 👊";
+  document.body.textContent = count + "👊";
 });
 
+document.body.addEventListener("touchmove", function (e) {
+  let count = e.touches.length;
+  document.body.textContent = fingers[count - 1];
+});
